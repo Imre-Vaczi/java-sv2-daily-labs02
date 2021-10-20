7e866d3dd5a4746686b66c23d877cf890ff55a97
Imre-Vaczi <imre.vaczi@gmail.com>
2021-10-20 18:21:08 +0200

    Geometry class updated

---
+++ src/main/java/day02/Geometry.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2.0, 8.0);
+        System.out.println("Side 'a' of the rectangle: " + rectangle.getaSide());
+        System.out.println("Side 'b' of the rectangle: " + rectangle.getbSide());
+        System.out.println("Area of the rectangle: " + rectangle.calculateArea(rectangle.getaSide(), rectangle.getbSide()));
+    }
+}
